692b8d30edf5ea13a3c200561bfa837043aa244d
Periyaperumal8898 <periyaperumalthala@gmail.com>
2025-08-24 23:23:17 +0530

    Update streamlit_app.py
    Added data frame to display json object in correct format

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,9 +44,7 @@ if ingredients_list:
       my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
       #st.write(my_insert_stmt)
-      import requests
-      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-      st.text(smoothiefroot_response)
+     
       time_to_insert = st.button('Submit Order')
       
 
@@ -55,7 +53,10 @@ if ingredients_list:
           
            st.success('Your Smoothie is ordered!', icon="✅")
          
-
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
 
